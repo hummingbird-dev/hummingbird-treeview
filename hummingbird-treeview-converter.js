7bfd05e0427f297307d08cf72465f2bbb1d0638b
Sebastian Mieruch <sebastian.mieruch@gmx.de>
2017-11-29 09:35:16 +0100

    development

--- hummingbird-treeview-converter.js
+++ hummingbird-treeview-converter.js
@@ -2,26 +2,45 @@ $(document).ready(function() {
 
 
     //hide simple treeview structure
-    $(".hummingbird-converter").hide();
+    $(".hummingbird-treeview-converter").hide();
 
     //create new treeview container
-    var template_header = '<div id="treeview_container" class="hummingbird-treeview" style="height: 230px; overflow-y: scroll;">'
-    var template_footer = "<div>"
-    $(".hummingbird-converter").after(template_header + template_footer);
+    var template_header = '<div id="treeview_container" class="hummingbird-treeview" style="height: 230px; overflow-y: scroll;">';
+    var template_subheader = '<ul id="treeview" class="hummingbird-base">';
+    $(".hummingbird-treeview-converter").after(template_header + template_subheader);
 
 
     //get treeview elements
-    var tree = $(".hummingbird-converter").children("li");
+    var tree = $(".hummingbird-treeview-converter").children("li");
 
     //loop through the elements and create tree
+    var id = 0;
+    var item = "";
     $.each(tree, function(i,e) {
 	var treeText = $(this).text();
 	//cut out leading -
 	var numHyphen = treeText.lastIndexOf("-");
-	console.log(numHyphen + " " + treeText)
-	
+	//console.log(numHyphen + " " + treeText)
+	var numHyphen_next = $(this).next().text().lastIndexOf("-");
+	//what is this, parent, children or sibling
+	//this is a parent
+	if (numHyphen < numHyphen_next) {
+	    item = item + '<li>' +"\n";
+	    item = item + '<i class="fa fa-plus"></i>' + "\n";
+	    item = item + '<label>' + "\n";
+	    item = item + '<input id="hum-' + id + '" data-id="' + treeText + '" type="checkbox" />' + treeText + "\n";
+	    item = item + '</label>' + "\n";
+	    item = item + '</li>' + "\n";
+	    //console.log(item);
+	    id++;
+	}
 	
     });
+    item = item + '</ul></div>';
+    console.log(item)
+    $(".hummingbird-base").after(item);
+
+
     
 
 
